31c7e04cca916fe9f400c348183d06644b53ea8a
computerkeeda <samwodson@gmail.com>
2024-12-03 07:45:23 +0530

    This is a change to add a struct called `Tweet` for storing tweet information. The contract also has methods to post and get tweets by user.
    1. Adds a new struct Tweet for storing tweet information.
    2. Provides methods to post and get tweets by user.</s>

--- contracts/Struct-Twitter.sol
+++ contracts/Struct-Twitter.sol
@@ -12,7 +12,12 @@ contract Twitter {
     mapping(address => Tweet[]) public tweetsByUser;
 
     function postTweet(uint256 _id, string memory _content) public {
-        Tweet memory newTweet = Tweet(_id, _content, msg.sender, block.timestamp);
+        Tweet memory newTweet = Tweet(
+            _id,
+            _content,
+            msg.sender,
+            block.timestamp
+        );
         tweetsByUser[msg.sender].push(newTweet);
     }
 
@@ -20,8 +25,16 @@ contract Twitter {
         return tweetsByUser[user];
     }
 
-    function getSingleTweet(address user, uint256 index) public view returns (Tweet memory) {
+    function getSingleTweet(
+        address user,
+        uint256 index
+    ) public view returns (Tweet memory) {
         return tweetsByUser[user][index];
     }
 
+    function getTweetLength(address user) public view returns (uint256) {
+        Tweet[] memory tweets = tweetsByUser[user];
+        uint256 length = tweets.length;
+        return length;
+    }
 }
